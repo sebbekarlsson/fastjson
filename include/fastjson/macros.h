141c5c62df50bcf2ddc787fa32ed8cd3f8e05c49
sebastian karlsson <sebbekarlsson97@gmail.com>
2023-05-31 16:40:16 +0200

    ifndef

--- include/fastjson/macros.h
+++ include/fastjson/macros.h
@@ -1,8 +1,13 @@
 #ifndef FAST_JSON_MACROS_H
 #define FAST_JSON_MACROS_H
 #include <stdlib.h>
+
+#ifndef OR
 #define OR(a, b) (a ? a : b)
+#endif
+#ifndef NEW
 #define NEW(T) ((T *)calloc(1, sizeof(T)))
+#endif
 
 #define FJSON_GENERATE_ENUM(ENUM) ENUM,
 #define FJSON_GENERATE_STRING(STRING) #STRING,
